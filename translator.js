adaac62faeca18a4d97d37748cf72f6406cd9f88
David Abram <David Abram>
2016-11-20 15:09:36 +0100

    addTranslation

--- translator.js
+++ translator.js
@@ -2,6 +2,33 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('translate.db');
 var async = require('async');
 
+function addWord(word, lang, callback) {
+
+    db.get(`SELECT * FROM ${lang} WHERE word == '${word}'`
+        , (err, result) => {
+            if (err == null) {
+
+                if (result == undefined) {
+                    db.run(`INSERT INTO ${lang} (word)
+                         VALUES ('${word}')`
+                        , function (err) {
+                            if (err == null) {
+                                callback(null, { id: this.lastID, lang: lang })
+                            } else {
+                                callback(err)
+                            }
+                        });
+
+                } else {
+                    callback(null, { id: result.id, lang: lang })
+                }
+            } else {
+                callback(err);
+            }
+        })
+
+}
+
 module.exports = {
     translate: (word, langFrom, langTo, callback) => {
         result = JSON.parse(`{ "${langFrom}": "${word}", "${langTo}": [] }`);
@@ -24,34 +51,48 @@ module.exports = {
                 }
             });
     },
-    addWord: (word, lang) =>{
-        
-        db.get(`SELECT * FROM ${langFrom} WHERE word == '${word}'`
-            , (err, result) => {
-                if (err == null) {
+    addTranslation: (word, translation, langFrom, langTo, callback) => {
+        async.parallel([
+            (sync) => {
+                addWord(word, langFrom, sync);
+            },
+            (sync) => {
+                addWord(translation, langTo, sync);
+            }
+        ], function (err, results) {
 
+            db.get(`SELECT * FROM translation WHERE ${results[0].lang}id == '${results[0].id}' AND ${results[1].lang}id == '${results[1].id}'`, (err, result) => {
+                if (err == null) {
                     if (result == undefined) {
-                        db.run(`INSERT INTO ${langFrom} (word)
-                         VALUES ('${word}')`
-                            , function(err) {
+                        db.get(`INSERT INTO translation (${results[0].lang}id, ${results[1].lang}id) VALUES ('${results[0].id}', ${results[1].id})`
+                            , (err, result) => {
                                 if (err == null) {
-                                    callback(null, {id: this.lastID})
+                                    callback(null);
                                 } else {
-                                    callback(err)
+                                    callback(err);
                                 }
                             });
-
                     } else {
-                        callback(null, {id: result.id})
+                        callback(null)
                     }
                 } else {
                     callback(err);
                 }
-            })
+            });
 
-    },
-    addTranslation: (word, translation, langFrom, langTo, callback) => {
-        
+            /*var a = `INSERT INTO translation (${results[0].lang}id, ${results[1].lang}id) VALUES ('${results[0].id}', ${results[1].id})`;
+
+            db.get(`INSERT INTO translation (${results[0].lang}id, ${results[1].lang}id) VALUES ('${results[0].id}', ${results[1].id})`
+            , (err, result) =>{
+                if(err == null){
+                    callback(null, result);
+                } else {
+                    callback(err);
+                }
+            });
+
+            callback(err, results);*/
+        });
 
     }
 }
